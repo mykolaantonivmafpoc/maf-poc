d531011e10293986dfc032575b5c9c7a2da00bf6
Eugene Yunak <yyunak@softserveinc.com>
2019-02-18 10:12:27 +0200

    api: fix api endpoint not sending body back (#29)

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 if [ -z "$@" ]; then
-  # exec python /app/migrations.py
-  exec gunicorn -k egg:meinheld#gunicorn_worker -c /gunicorn_conf.py "app.run:app"
+  exec gunicorn -c /gunicorn_conf.py "app.run:app"
 else
   exec "$@"
 fi
--- docker/gunicorn_conf.py
+++ docker/gunicorn_conf.py
@@ -1,5 +1,5 @@
-loglevel = 'info'
-workers = 2
+loglevel = 'debug'
+workers = 8
 bind = '0.0.0.0:80'
 keepalive = 120
 errorlog = '-'
